868d1c45362366d9cb1c50f0dcc856438da6f237
Egor19035 <egororachyov5@gmail.com>
2021-03-27 16:45:15 +0300

    [Code] Minor fixes

--- rfsim/sources/Simulator.cpp
+++ rfsim/sources/Simulator.cpp
@@ -46,7 +46,7 @@ namespace rfsim {
         // Setup main window and window manager
         mWindowManager = std::make_shared<WindowManager>();
         mPrimaryWindow = mWindowManager->CreateNewWindow({1280, 720}, "Robot Football Sim");
-        mPainter = std::make_shared<PainterEngine>(glm::ivec4{0, 0, 1280, 720}, glm::vec4{0, 0, 1280, 720}, mPrimaryWindow);
+        mPainter = std::make_shared<Painter>(glm::ivec4{0, 0, 1280, 720}, glm::vec4{0, 0, 1280, 720}, mPrimaryWindow);
         mGraphicsServer = std::make_shared<GraphicsServer>(mPrimaryWindow, mPainter, mResourcesPath);
         mPhysicsServer = std::make_shared<PhysicsServer>();
         mAlgorithmManager = std::make_shared<AlgorithmManager>(mPluginsPath);
